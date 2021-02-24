29d4c6655ea3dfd5d4c87125a675ae7660ab8a95
Canzalo <44494765+canzalo@users.noreply.github.com>
2021-02-24 16:10:01 +0300

    Create client_in_app.js

---
+++ client_in_app.js
@@ -0,0 +1,30 @@
+
+import React from 'react';
+import { ApolloProvider } from '@apollo/react-hooks';
+import client from './client';
+import {Typography, Container, makeStyles} from "@material-ui/core";
+import RepositoryList from "./RepositoryList";
+import SearchBar from "./SearchBar";
+
+const useStyles = makeStyles({
+  title: {
+    marginTop: '1rem',
+    marginBottom: '1rem',
+    textAlign: 'center'
+  }
+});
+
+const App = () => {
+  const classes = useStyles();
+  return (
+    <ApolloProvider client={client}>
+      <Container maxWidth={'sm'}>
+        <Typography variant={'h3'} className={classes.title}>GraphQL Github Client</Typography>
+        <SearchBar />
+        <RepositoryList />
+      </Container>
+    </ApolloProvider>
+  );
+};
+
+export default App;
