c42b42e86c506af072705fcda336ee736a5081af
Canzalo <44494765+canzalo@users.noreply.github.com>
2021-02-24 16:05:44 +0300

    Create repositoryList.js

---
+++ repositoryList.js
@@ -0,0 +1,26 @@
+
+import React from 'react';
+import {Typography, makeStyles} from "@material-ui/core";
+
+const useStyles = makeStyles({
+  noRepoNote: {
+    marginTop: '1rem',
+    textAlign: 'center',
+  }
+});
+
+const RepositoryList = ({searchTerm}) => {
+  const classes = useStyles();
+
+  return (
+    <Typography 
+      variant={'overline'} 
+      className={classes.noRepoNote} 
+      component={'div'}
+    >
+      No repositories yet.
+    </Typography>
+  );
+};
+
+export default RepositoryList;
